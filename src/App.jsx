0946e5a83fdc33f53a3ba2d0702f8558afa730bb
Shifali Gwal <gwal.shifali2223@gmail.com>
2021-09-26 23:50:42 +0530

    Add Portfolio Section

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <div className="sections">
         <Intro />
         <Portfolio />
-        <Works />
+        {/* <Works /> */}
         <Testimonials />
         <Contact />
       </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,83 +1,34 @@
 import { useEffect, useState } from "react";
-import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
-import {
-  featuredPortfolio,
-  webPortfolio,
-  mobilePortfolio,
-  designPortfolio,
-  contentPortfolio,
-} from "../../data";
+import { GitHub } from "@material-ui/icons";
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState("featured");
   const [data, setData] = useState([]);
-  const list = [
-    {
-      id: "featured",
-      title: "Featured",
-    },
-    {
-      id: "web",
-      title: "Web App",
-    },
-    {
-      id: "mobile",
-      title: "Mobile App",
-    },
-    {
-      id: "design",
-      title: "Design",
-    },
-    {
-      id: "content",
-      title: "Content",
-    },
-  ];
 
+  const fetchData = async () => {
+    const data = await fetch("https://api.github.com/users/theshifaligwal/repos").then((response) => response.json());
+    // Filter forked repos
+    data.filter((repo) => !repo.fork);
+    // Sort repos according to there update time
+    data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+    const newData = data.slice(0, 6);
+    setData(newData);
+    console.log(newData);
+  }
   useEffect(() => {
-    switch (selected) {
-      case "featured":
-        setData(featuredPortfolio);
-        break;
-      case "web":
-        setData(webPortfolio);
-        break;
-      case "mobile":
-        setData(mobilePortfolio);
-        break;
-      case "design":
-        setData(designPortfolio);
-        break;
-      case "content":
-        setData(contentPortfolio);
-        break;
-      default:
-        setData(featuredPortfolio);
-    }
-  }, [selected]);
+    fetchData();
+  }, []);
 
   return (
     <div className="portfolio" id="portfolio">
-      <h1>Portfolio</h1>
-      <ul>
-        {list.map((item) => (
-          <PortfolioList
-            title={item.title}
-            active={selected === item.id}
-            setSelected={setSelected}
-            id={item.id}
-          />
-        ))}
-      </ul>
+      <h1>Current Projects</h1>
       <div className="container">
-        {data.map((d) => (
-          <div className="item">
-            <img
-              src={d.img}
-              alt=""
-            />
-            <h3>{d.title}</h3>
+        {data.map((repo) => (
+          <div className="item-repos" key={repo.id}>
+            <a href={repo.url} class="icon-github">
+              <GitHub />
+              <h3>{repo.name}</h3>
+            </a>
           </div>
         ))}
       </div>
