d53b64413ec1edf084429341a1e2c4357f691f4f
Shifali Gwal <gwal.shifali2223@gmail.com>
2021-09-26 21:11:04 +0530

    Add Intro

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -19,7 +19,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
