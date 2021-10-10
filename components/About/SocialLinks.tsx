d86236456a1ab5a876f9059ea8dbb40ca4993e66
theshifaligwal <gwal.shifali2223@gmail.com>
2021-10-10 10:55:39 +0530

    Update social links

--- components/About/SocialLinks.tsx
+++ components/About/SocialLinks.tsx
@@ -4,7 +4,6 @@ import style from "../../styles/components/SocialLinks.module.css";
 // Icons
 import {
   AiFillGithub,
-  AiFillInstagram,
   AiFillLinkedin,
   AiFillTwitterCircle,
 } from "react-icons/ai";
@@ -12,11 +11,6 @@ import {
 const SocialLinks = () => {
   return (
     <ul className={style.socialLink}>
-      <li className={style.instagram}>
-        <a href="https://www.instagram.com/shifali_gwal/">
-          <AiFillInstagram size="20px" />
-        </a>
-      </li>
       <li className={style.github}>
         <a href="https://github.com/theshifaligwal">
           <AiFillGithub size="20px" />
